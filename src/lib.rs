6a1748084ec572d5c1628d51879a55619277abd7
laurirasanen <lauri@rasanen.dev>
2023-01-27 23:16:57 +0200

    wasm WIP

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+#![allow(clippy::needless_return)]
+
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+mod core;
+mod shaders;
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
+pub fn run() {
+    #[cfg(target_arch = "wasm32")]
+    {
+        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+        console_log::init_with_level(log::Level::Warn).expect("Couldn't initialize logger");
+        wasm_bindgen_futures::spawn_local(core::window::run());
+    }
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        env_logger::init();
+        pollster::block_on(core::window::run());
+    }
+}
